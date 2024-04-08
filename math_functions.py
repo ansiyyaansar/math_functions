bff783948ae93b01e93463b204ac1f147b852aef
ansiyyaansar <ansiyyaansar@gmail.com>
2024-04-08 11:24:09 +0530

    math functions

---
+++ math_functions.py
@@ -0,0 +1,103 @@
+import math
+
+def sqrt():
+    number=int(input("enter your number : "))
+    square_root = math.sqrt(number)
+    print(square_root)
+
+# sqrt()
+
+def cos():
+    number=int(input("enter your number :"))
+    mathcos=math.cos(number)
+    print(mathcos)
+
+# cos()
+    
+
+def degree():
+    number=int(input("enter your number : "))
+    math_degrees=math.degrees(number)
+    print(math_degrees) 
+
+# degree()
+
+def comb():
+    number1=int(input("enter your number : "))
+    number2=int(input("enter your number : "))
+    math_comb=math.comb(number1,number2)
+    print(math_comb)
+# comb()
+
+def power():
+    number1=int(input("enter your number1 : "))
+    number2=int(input("enter your number : "))
+    number_power=math.pow(number1,number2)
+    print(number_power)
+# power()
+    
+def fact():
+    number=int(input("enter your number : "))
+    factorial=math.factorial(number)
+    print(factorial)
+# fact()
+
+def isclose():
+    number1=int(input("enter your number1 : "))
+    number2=int(input("enter your number2 : "))
+    math_isclose=math.isclose(number1,number2)
+    print(math_isclose)
+# isclose()
+
+def greatest_divider():
+    number1=int(input("enter your number1 : "))
+    number2=int(input("enter your number2 : "))
+    greatest_common_divider=math.gcd(number1,number2)
+    print(greatest_common_divider)
+# greatest_divider()
+
+def copy_sighn():
+    number1=int(input("enter your number1 : "))
+    number2=int(input("enter your number2 : "))
+    copysign=math.copysign(number1,number2)
+    print(copysign)
+# copy_sighn()
+    
+def f_mod():
+    number1=int(input("enter your number1 : "))
+    number2=int(input("enter your number2 : "))
+    mathfmod=math.fmod(number1,number2)
+    print(mathfmod)
+# f_mod()
+    
+def radian():
+    number=int(input("enter your number : "))
+    radians=math.radians(number)
+    print(radians)
+# radian()
+
+def log():
+    number=int(input("enter your number : "))
+    mathlog=math.log(number)
+    print(mathlog)
+# log()
+
+def ceil_():
+    number=float(input("enter your number : "))
+    mathceil=math.ceil(number)
+    print(mathceil)
+# ceil_()   
+
+def floor_():
+    number=float(input("enter your number : "))
+    mathfloor=math.floor(number)
+    print(mathfloor)
+
+# floor_()    
+
+def fabs_():
+    number=float(input("enter your number : "))
+    mathfabs=math.fabs(number)
+    print(mathfabs)
+
+# fabs_()  
